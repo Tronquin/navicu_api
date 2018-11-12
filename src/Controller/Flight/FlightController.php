4c51c954ca1f3c3eea9345c69e1b04b735cc88ac
Emilio Ochoa <eochoa@navicu.com>
2018-11-12 14:26:07 -0400

    Servicio para obtener listado de cabinas

--- src/Controller/Flight/FlightController.php
+++ src/Controller/Flight/FlightController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Flight;
 
 use App\Navicu\Handler\Flight\AutocompleteHandler;
+use App\Navicu\Handler\Flight\CabinHandler;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,7 +15,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class FlightController extends AbstractController
 {
     /**
-     * @Route("/autocomplete/{words}", name="flight_autocomplete")
+     * @Route("/autocomplete/{words}", name="flight_autocomplete", methods="GET")
      *
      * @param Request $request
      * @return JsonResponse
@@ -22,6 +23,20 @@ class FlightController extends AbstractController
     public function index(Request $request)
     {
         $handler = new AutocompleteHandler($request);
+        $handler->processHandler();
+
+        return $handler->getJsonResponseData();
+    }
+
+    /**
+     * @Route("/cabins", name="flight_cabins", methods="GET")
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function cabins(Request $request)
+    {
+        $handler = new CabinHandler($request);
         $handler->processHandler();
 
         return $handler->getJsonResponseData();
