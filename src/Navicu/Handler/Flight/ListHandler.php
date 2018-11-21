cbf02c177063520a81acd2a6f7ba41f3586bf2ff
javert <jvasquez@navicu.com>
2018-11-21 17:40:10 -0400

    cambios en el listado de vuelos

--- src/Navicu/Handler/Flight/ListHandler.php
+++ src/Navicu/Handler/Flight/ListHandler.php
@@ -5,12 +5,14 @@ namespace App\Navicu\Handler\Flight;
 use App\Entity\Flight;
 use App\Entity\Airline;
 use App\Entity\Airport;
+use App\Entity\Consolidator;
 use App\Entity\AirlineTypeRate;
 use App\Navicu\Exception\NavicuException;
 use App\Navicu\Exception\OtaException;
 use App\Navicu\Handler\BaseHandler;
 use App\Navicu\Service\OtaService;
 use App\Navicu\Service\NavicuFlightConverter;
+use App\Navicu\Service\ConsolidatorService;
 
 /**
  * Listado de vuelos
@@ -28,8 +30,9 @@ class ListHandler extends BaseHandler
     protected function handler() : array
     {
 
-        $manager = $this->container->get('doctrine')->getManager();
         $params = $this->getParams();
+        $manager = $this->container->get('doctrine')->getManager();
+        $consolidator = $manager->getRepository(Consolidator::class)->getFirstConsolidator();
 
         if ($params['roundTrip'] == 0) {
             $resp = 'oneWay';
@@ -42,49 +45,51 @@ class ListHandler extends BaseHandler
 
         if ($response['code'] !== OtaService::CODE_SUCCESS) {
             throw new OtaException($response['errors']);
-        }  
-
-   
+        }     
          
         $segments = [];
         foreach ($response[$resp] as $key => $segment) {
-            
-            $segment['amounts'] = [];
-            $segment['amounts']['original_price'] = $segment['price'];
 
-            $negotiatedRate = false;
-            foreach ($segment['flights'] as $key => $flight) {
-                $negotiatedRate = $flight['negotiated_rate'];
-                if ($negotiatedRate) {
-                    break;
+            if (!$consolidator || ($segment['price'] > $consolidator->getCreditAvailable())) {  
+
+                $segment['amounts'] = [];
+                $segment['amounts']['original_price'] = $segment['price'];
+
+                $negotiatedRate = false;
+                foreach ($segment['flights'] as $key => $flight) {
+                    $negotiatedRate = $flight['negotiated_rate'];
+                    if ($negotiatedRate) {
+                        break;
+                    }
                 }
+
+                $flightLockDate = new \DateTime($segment['flights'][0]['departure']);
+
+                $convertedAmounts = NavicuFlightConverter::calculateFlightAmount($segment['price'], $params['currency'],
+                        [   'iso' => $segment['flights'][0]['airline'],
+                            'rate' => $segment['flights'][0]['rate'],
+                            'from' => $segment['flights'][0]['origin'],
+                            'to' => $segment['flights'][0]['destination'],
+                            'departureDate' => $flightLockDate
+                        ],$params['userCurrency'], 
+                        [
+                            'provider' => $params['provider'],
+                            'negotiatedRate' =>  $negotiatedRate,                    
+                        ]
+                    );
+                
+                $segment['amounts']['price'] = $convertedAmounts['subTotal'];
+                $segment['amounts']['original_price_no_tax'] = $segment['priceNoTax'];
+                $segment['amounts']['incrementLock'] = $convertedAmounts['subTotal'];
+                $segment['amounts']['incrementConsolidator'] = $convertedAmounts['incrementConsolidator'];
+                $segment['amounts']['incrementMarkup'] = $convertedAmounts['incrementMarkup'];
+                $segment['amounts']['incrementExpenses'] = $convertedAmounts['incrementExpenses'];
+                $segment['amounts']['incrementGuarantee'] = $convertedAmounts['incrementGuarantee'];
+                $segment['amounts']['discount'] = $convertedAmounts['discount'];
+                $segment['amounts']['tax'] = $convertedAmounts['tax'];
+                $segments[] = $segment;
+
             }
-
-            $flightLockDate = new \DateTime($segment['flights'][0]['departure']);
-
-            $convertedAmounts = NavicuFlightConverter::calculateFlightAmount($segment['price'], $params['currency'],
-                    [   'iso' => $segment['flights'][0]['airline'],
-                        'rate' => $segment['flights'][0]['rate'],
-                        'from' => $segment['flights'][0]['origin'],
-                        'to' => $segment['flights'][0]['destination'],
-                        'departureDate' => $flightLockDate
-                    ],$params['userCurrency'], 
-                    [
-                        'provider' => $params['provider'],
-                        'negotiatedRate' =>  $negotiatedRate,                    
-                    ]
-                );
-            
-            $segment['amounts']['price'] = $convertedAmounts['subTotal'];
-            $segment['amounts']['original_price_no_tax'] = $segment['priceNoTax'];
-            $segment['amounts']['incrementLock'] = $convertedAmounts['subTotal'];
-            $segment['amounts']['incrementConsolidator'] = $convertedAmounts['incrementConsolidator'];
-            $segment['amounts']['incrementMarkup'] = $convertedAmounts['incrementMarkup'];
-            $segment['amounts']['incrementExpenses'] = $convertedAmounts['incrementExpenses'];
-            $segment['amounts']['incrementGuarantee'] = $convertedAmounts['incrementGuarantee'];
-            $segment['amounts']['discount'] = $convertedAmounts['discount'];
-            $segment['amounts']['tax'] = $convertedAmounts['tax'];
-            $segments[] = $segment;
 
         }    
 
@@ -92,6 +97,61 @@ class ListHandler extends BaseHandler
         $response['segments'] = $segments; 
 
         return $response;
+    }
+
+
+    /**
+     * Verifica si existe el logo de las aerolineas
+     *
+     * @param array $data
+     * @return array
+     */
+    private function logoAirlineExists($data)
+    {
+        global $kernel;
+        $dir = $kernel->getRootDir() . '/../web/images/airlines/';
+
+        if (isset($data['segments'])) {
+            foreach ($data['segments'] as $k => $segment) {
+                foreach ($segment['flights'] as $j => $flight) {
+                    
+                    $data['segments'][$k]['flights'][$j]['logo_exists'] = file_exists($dir . $flight['airline'] . '.png');
+                }                
+            }
+        }        
+
+        return $data;
+    }
+
+
+    /**
+     * Registra una aerolinea
+     *
+     * @param array $data
+     * @return Airline
+     */
+    private function createAirline(array $data)
+    {
+
+        $manager = $this->container->get('doctrine')->getManager(); 
+
+        $airline = new Airline();
+
+        $airline
+            ->setIso($data['airline'])
+            ->setName($data['airlineName'])
+            ->setVisible(true)
+            ->setStatus(1)
+            ->setCreditAvailable(0)
+            ->setCreditInitial(0)
+            ->setInvoiceDays(1)
+            ->setPaymentDays(1)
+            ->setInvoiceType(1);
+
+        $manager->persist($airline);
+        $manager->flush();
+
+        return $airline;
     }
 
 
