13a41c1e8e7956ebce7fa6a615c979ef60788467
Ruben Hidalgo <rhidalgo@jacidi.com>
2019-02-08 09:22:53 -0400

    cambio de controllador metodo socialService

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Navicu\Handler\Security\UserExistsHandler;
 use App\Navicu\Handler\Security\RegisterUserSimpleHandler;
+use App\Navicu\Handler\SocialServices\SocialServiceHandler;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -41,6 +42,22 @@ class SecurityController extends AbstractController
         $handler->setParam('encoder', $encoder);
         $handler->processHandler();
 
+        return $handler->getJsonResponseData();
+    }
+
+    /**
+     * Devuelve el token secreto de las redes sociales (google, facebook,etc)
+     *
+     * @Route("/social_provider_data", name="socialproviderdata", methods="POST")
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function socialProviderData(Request $request)
+    {
+        $handler = new SocialServiceHandler($request);
+        $handler->processHandler();
+        
         return $handler->getJsonResponseData();
     }
 
