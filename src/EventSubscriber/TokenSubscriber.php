75000429c7f0870fa09fe82d9d6058353c82e15c
Jalevf <javier.avfs@gmail.com>
2019-02-14 09:11:33 -0400

    descomentando tokenSuscriber

--- src/EventSubscriber/TokenSubscriber.php
+++ src/EventSubscriber/TokenSubscriber.php
@@ -21,7 +21,7 @@ class TokenSubscriber implements EventSubscriberInterface
     public function onKernelController(FilterControllerEvent $event)
     {
         
-      /*$request = $event->getRequest();
+      $request = $event->getRequest();
     
       if (! $request->headers->has('token')) {
 
@@ -52,7 +52,7 @@ class TokenSubscriber implements EventSubscriberInterface
                 return new JsonResponse(['code' => 400, 'errors' => ['token expired']]);
             });
         }
-           */    
+               
     }
 
     public static function getSubscribedEvents()
