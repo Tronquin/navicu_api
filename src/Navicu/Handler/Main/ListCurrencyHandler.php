2c67d1c7ae281e61727e139b8c85adb52bf2778e
vito cervelli <vcervelli@navicu.com>
2019-06-07 16:56:56 -0400

    % ticket 796

--- src/Navicu/Handler/Main/ListCurrencyHandler.php
+++ src/Navicu/Handler/Main/ListCurrencyHandler.php
@@ -25,14 +25,25 @@ class ListCurrencyHandler extends BaseHandler
         $currency = $manager->getRepository(CurrencyType::class)->findBy(['active'=>true]);
 
         $response = [];
+        $order= [];
         foreach ($currency as $coin) {
             $objCurrency['alpha'] = $coin->getAlfa3();
             $objCurrency['title'] = $coin->getTitle();
             $objCurrency['sym'] = (!empty($coin->getSimbol())) ? $coin->getSimbol() : "";
-            array_push($response, $objCurrency);
+           
+                array_push($response, $objCurrency);
+            
+           
         }
-
-
+      
+        //Colocando elementos en primera posicion
+        $currenctVES = $response[1];
+        unset($response[1]);
+        $currenctUSD = $response[2];
+        unset($response[2]);
+        $currenctEUR = $response[3];
+        unset($response[3]);
+        array_unshift($response, $currenctVES, $currenctUSD, $currenctEUR );
         return $response;
     }   
 
